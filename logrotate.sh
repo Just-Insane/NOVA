8b96784070255be326c30217df0a445ae73c709d
Justin Gauthier <justin@justin-tech.com>
2018-05-12 12:55:26 -0400

    Add log rotation

---
+++ logrotate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+d=$(date '+%Y-%m-%d-%s')
+log=/var/log/NOVA/NOVA.log
+dlog=/var/log/NOVA/NOVA-$d.log
+MaxFileSize=2048
+while true
+do
+	sh /home/pi/scripts/startup.sh >> $log
+	#Get Size in Bytes
+	file_size=`du -b $log | tr -s '\t' ' ' | cut -d' ' -f1`
+	if [ $file_size -gt $MaxFileSize ];
+	then
+		mv $log $dlog
+		touch $log
+	fi
+done
--- startup.sh
+++ startup.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
 d=$(date '+%Y-%m-%d')
 dlong=$(date)
-log=/var/log/NOVA/NOVA-$d.log
+log=/var/log/NOVA/NOVA.log
+dlog=/var/log/NOVA/NOVA-$d.log
 i="0"
 
-touch $log
-echo "Starting NOVA script on $dlong" >> $log
+echo "Starting NOVA script on $dlong" #>> $log
 sleep 10
 
 if pgrep "ppp" > /dev/null
 then
-	echo "Running" >> $log
+	echo "PPP Process Running" #>> $log
 	status="Running"
 else
-	echo "Stopped" >> $log
+	echo "PPP Process Stopped" #>> $log
 	status="Not Running"
 	sleep 10
-	sudo /usr/local/bin/hologram modem connect >> $log
+	sudo /usr/local/bin/hologram modem connect #>> $log
 fi
 sleep 10
-sudo hologram send "Startup on $dlong, Hologram was $status." >> $log
+sudo hologram send "Startup on $dlong, Hologram was $status." #>> $log
 
 while [ $i -lt 1 ]; do
 if ip route | grep ppp > /dev/null
 then
+	echo "Route is UP!" #>> $log
 	sleep 60
 else
-	sudo /usr/local/bin/hologram modem disconnect >> $log
+	sudo /usr/local/bin/hologram modem disconnect #>> $log
 	sleep 10
-	sudo /usr/local/bin/hologram modem connect >> $log
+	sudo /usr/local/bin/hologram modem connect #>> $log
+fi
+if [ ! -f $dlog ];
+then
+	echo "File not found!" #>> $log
+else
+	echo "File Found!" #>> $log
+	i++
 fi
 done
